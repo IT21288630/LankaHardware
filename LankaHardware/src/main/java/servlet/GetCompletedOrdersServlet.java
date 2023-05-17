4af9c3a973fe41b2ac7d69ae01dec08d8f2941ba
IT21288630 <it21288630@my.sliit.lk>
2023-05-18 00:15:14 +0530

    Completed orders

---
+++ LankaHardware/src/main/java/servlet/GetCompletedOrdersServlet.java
@@ -0,0 +1,56 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+import service.CheckoutServiceImpl;
+import service.ICheckoutService;
+
+/**
+ * Servlet implementation class GetCompletedOrdersServlet
+ */
+@WebServlet("/GetCompletedOrdersServlet")
+public class GetCompletedOrdersServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GetCompletedOrdersServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		ICheckoutService iCheckoutService = new CheckoutServiceImpl();
+		
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+		PrintWriter out = response.getWriter();
+
+		String resp = new Gson().toJson(iCheckoutService.getCompletedOrders("a@g.m"));
+		
+		out.print(resp);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
